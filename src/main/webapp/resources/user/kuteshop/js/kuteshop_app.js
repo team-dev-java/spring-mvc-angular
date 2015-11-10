1ba36f35a8135fc18cfa4c0fa66a587fd54b0241
mrdquan <mrdquan89@gmail.com>
2015-11-10 09:59:16 +0700

    commit
    commit template

--- src/main/webapp/resources/user/kuteshop/js/kuteshop_app.js
+++ src/main/webapp/resources/user/kuteshop/js/kuteshop_app.js
@@ -1,1 +1,12 @@
-var kuteshop_app = angular.module('kuteshopApp', ['ngRoute']);
+var kuteshop_app = angular.module('kuteshopApp', [ 'ngRoute' ]);
+kuteshop_app.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/getList', {
+		templateUrl : 'partials/phone-list.html',
+		controller : 'PhoneListCtrl'
+	}).when('/phones/:phoneId', {
+		templateUrl : 'partials/phone-detail.html',
+		controller : 'PhoneDetailCtrl'
+	}).otherwise({
+		redirectTo : '/home'
+	});
+} ])
